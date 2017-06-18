bd95e9501fbbfbe8a0af5a60596af213008ee52d
Rath Vong <rathvong@mekongmessenger.com>
2017-06-18 12:27:10 -0400

    latest build

--- slidescreenlibrary/src/main/java/mekong/slidescreenlibrary/SlideScreenActivity.java
+++ slidescreenlibrary/src/main/java/mekong/slidescreenlibrary/SlideScreenActivity.java
@@ -4,16 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
 
 import java.util.ArrayList;
 
@@ -22,12 +21,12 @@ import mekong.slidescreenlibrary.Fragments.ScreenFragment;
 public class SlideScreenActivity extends AppCompatActivity {
     public static final int RESULT_FINISHED = 10001;
     private ViewPager viewPager;
+    private TabLayout tabLayout;
 
-    private LinearLayout dotsLayout;
-    private TextView[] dots;
     private ArrayList<ScreenData> layouts;
     private Button btnSkip, btnNext;
     private SlideOptions slideOptions;
+
 
 
     public static void start(Activity activity, ArrayList<ScreenData> layouts){
@@ -65,7 +64,7 @@ public class SlideScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_slide_screen);
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
-        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         btnSkip = (Button) findViewById(R.id.btn_skip);
         btnNext = (Button) findViewById(R.id.btn_next);
 
@@ -76,10 +75,7 @@ public class SlideScreenActivity extends AppCompatActivity {
         btnNext.setText(layouts.get(0).getRightButtonText());
 
 
-        // adding bottom dots
-        addBottomDots(0);
-
-        // making notification bar transparent
+      // making notification bar transparent
         changeStatusBarColor();
 
         setupViewPager(viewPager);
@@ -128,27 +124,18 @@ public class SlideScreenActivity extends AppCompatActivity {
 
         viewPager.setAdapter(myViewPagerAdapter);
         viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+        tabLayout.setupWithViewPager(viewPager, true);
+        tabLayout.setSelectedTabIndicatorColor(layouts.get(0).getDotsActiveColor());
+
+        for (int i = 0; i < layouts.size(); i++) {
+                tabLayout.getTabAt(i).setText("");
+           }
+
 
     }
 
 
-    private void addBottomDots(int currentPage) {
-        dots = new TextView[layouts.size()];
 
-
-
-        dotsLayout.removeAllViews();
-        for (int i = 0; i < dots.length; i++) {
-            dots[i] = new TextView(this);
-            dots[i].setText(Html.fromHtml("&#8226;"));
-            dots[i].setTextSize(35);
-            dots[i].setTextColor(layouts.get(currentPage).getDotsInActiveColor());
-            dotsLayout.addView(dots[i]);
-        }
-
-        if (dots.length > 0)
-            dots[currentPage].setTextColor(layouts.get(currentPage).getDotsActiveColor());
-    }
 
     private int getItem(int i) {
         return viewPager.getCurrentItem() + i;
@@ -165,19 +152,21 @@ public class SlideScreenActivity extends AppCompatActivity {
 
         @Override
         public void onPageSelected(int position) {
-            addBottomDots(position);
+
 
             // changing the next button text 'NEXT' / 'GOT IT'
             if (position == layouts.size() - 1) {
                 // last page. make button text to GOT IT
                 btnNext.setText(layouts.get(position).getLastButtonText());
-
                 btnSkip.setVisibility(View.GONE);
             } else {
                 // still pages are left
                 btnNext.setText(layouts.get(position).getRightButtonText());
                 btnSkip.setVisibility(View.VISIBLE);
             }
+
+            tabLayout.setSelectedTabIndicatorColor(layouts.get(position).getDotsActiveColor());
+
             btnSkip.setTextColor(layouts.get(position).getLeftButtonColors());
             btnNext.setTextColor(layouts.get(position).getRightButtonColors());
         }
